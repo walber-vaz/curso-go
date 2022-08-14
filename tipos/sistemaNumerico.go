e1a4ecf621a723b2d507170f108bd6f6f3ee76c5
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-14 16:59:40 -0300

    Curso de Go - Tipos sistema numericos - Binario, decimal e hexadecimal

---
+++ tipos/sistemaNumerico.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Sistema numérico -> Decimal, binário e Hexadecimal
+	*/
+
+	// %d -> decimal
+	// %b -> binário
+	// %#x -> Hexadecimal
+
+	a := 10
+	fmt.Printf("%d\t%b\t%#x\n", a, a, a)
+}

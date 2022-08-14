4b34d3fb9ac419524cfd28fe1b1c73740b9887e7
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-14 19:28:24 -0300

    Curso de Go - Exercicio nivel 2 - 3

---
+++ exercicios/nivel-2/ex3.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const a = 10
+const b int = 10
+
+func main() {
+	/*
+		Crie constantes tipadas e não tipadas.
+		Demostre seus valores.
+	*/
+
+	fmt.Printf("%v, %v\n", a, b)
+}

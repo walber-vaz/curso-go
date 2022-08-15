13d35f5dfdad202a85133bc5f869669ce00c6a97
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-15 10:46:27 -0300

    Curso Go - Exercicio nivel 2 - 5

---
+++ exercicios/nivel-2/ex5.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Crie uma variável de tipo string utilizando uma raw string literal
+	*/
+	s := `E uma raw
+				string
+					literal`
+	fmt.Println(s)
+}
